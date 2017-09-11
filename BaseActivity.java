aef23f412c382c48feff971e5483ca44fcdffc79
xinhua.shi <xinhua.shi@palmaplus.com>
2017-09-11 15:24:56 +0800

    二期功能 代码结构优化

--- BaseActivity.java
+++ BaseActivity.java
@@ -1,8 +1,69 @@
 package com.example.administrator.arnavigatedemo;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.administrator.arnavigatedemo.di.ActivityModule;
+import com.example.administrator.arnavigatedemo.di.DaggerActivityComponent;
+import com.example.administrator.arnavigatedemo.presenter.BleScanResultPresenter;
+import com.example.administrator.arnavigatedemo.view.MainActivity;
+import com.example.administrator.arnavigatedemo.widget.ProgressDialogDelegate;
+
+import javax.inject.Inject;
+
+import dagger.Lazy;
+
 /**
- * Created by Administrator on 2017/9/11/011.
+ * Created by Administrator on 2017/9/7/007.
  */
 
-public class BaseActivity {
+public class BaseActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        DaggerActivityComponent.builder().activityModule(new ActivityModule(this)).build().inject(this);
+    }
+
+    @Inject
+    protected Lazy<ProgressDialogDelegate> proDelegate;
+
+    public void showProDialog() {
+        proDelegate.get().show();
+    }
+
+    public void showProDialog(String title, String msg){
+        proDelegate.get().show(title, msg);
+    }
+
+    public void hideProDialog() {
+        try {
+            proDelegate.get().hide();
+        }catch (Exception e) {
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        try {
+            proDelegate.get().hide();
+            proDelegate = null;
+        } catch (Exception e) {
+        }
+    }
+
+    public void showLoading() {
+        showProDialog();
+    }
+
+    public void showLoading(String title, String msg) {
+        showProDialog(title, msg);
+    }
+
+    public void hideLoading() {
+        hideProDialog();
+    }
+
 }
