c44c26cb02a8b3c9bff37bb7e5c1cf4a2a983d81
xinhua.shi <xinhua.shi@palmaplus.com>
2017-09-13 11:34:17 +0800

    二期功能 代码结构优化

--- http/GetBeaconInfosService.java
+++ http/GetBeaconInfosService.java
@@ -1,19 +1,10 @@
 package com.example.administrator.arnavigatedemo.http;
 
 import com.example.administrator.arnavigatedemo.model.BeaconInfo;
-import com.example.administrator.arnavigatedemo.model.GetBeaconsInfo;
-
 import java.util.List;
-
-import okhttp3.RequestBody;
 import retrofit2.Call;
-import retrofit2.http.Body;
 import retrofit2.http.GET;
-import retrofit2.http.Headers;
-import retrofit2.http.POST;
-import retrofit2.http.Path;
 import retrofit2.http.Query;
-import rx.Single;
 
 /**
  * Created by Administrator on 2017/8/16/016.
